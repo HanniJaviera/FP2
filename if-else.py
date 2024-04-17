b5aa9f0413f079d572259f33afb804c7a1912702
HanniJaviera <javiera.arc@hotmail.com>
2024-04-16 21:02:18 -0400

    1604

---
+++ if-else.py
@@ -0,0 +1,9 @@
+number=7
+edad=5
+if number > edad:
+    print ("La variable booleana es verdadera.")
+elif edad > 50: 
+    print ("La suma es mayor que 50.")
+else: 
+    print ("Ninguna de las condiciones anteriores se cumplen")
+
---
+++ num-mayor.py
@@ -0,0 +1,10 @@
+#Cual de los 3 es mayor
+num1=10
+num2=12
+num3=3
+if num1 > num2 and num1>num3:
+    print ("El número mayor es", num1)
+elif num2>num3: 
+        print ("el numero mayor es", num2)
+else :
+        print ("el número mayor es", num3)
