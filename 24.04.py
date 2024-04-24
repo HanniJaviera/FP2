5874da64a74c8f29af2dcaaf6f23fb64d3f0126e
Hannijaviera <javiera.arc@hotmail.com>
2024-04-24 19:50:27 -0400

    mejora

--- 24.04.py
+++ 24.04.py
@@ -1,6 +1,6 @@
 # Escribir un programa que pida al usuario un número entero positivo y 
-#muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+#muestre por pantalla todos los números pares desde 2 hasta ese número separados por comas.
 n = int(input("Introduce un número entero positivo: "))
-for i in range(1, n+1, 2): #obligatorio es el primero que va hacer si o si, y es el limite inferior y el segundo el limite superior
+for i in range(1, n+1, 2): #obligatorio es el primero que va hacer si o si, y es el limite inferior y el segundo el limite superior, el tercero le suma es decir salto en cada interacion
     print(i, end=", ") #end hace que los numeros vayan para el lado en vez de hacia abajo
 
