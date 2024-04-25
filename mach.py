04befeca3aa03c81a8d3077c6f8ef36e9d83f54d
Hannijaviera <javiera.arc@hotmail.com>
2024-04-24 20:15:56 -0400

    si

---
+++ mach.py
@@ -0,0 +1,11 @@
+
+op=2
+match op:
+    case 1:
+        print("Esta es la opción 1")
+    case 2:
+        primerwhile()
+    case 3:
+        verifpassw()
+    case 4:
+        print ("Usted salio del programa")
