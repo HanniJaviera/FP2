673dead6c94ee11f96aa43e194cd58c3ff293066
hannijaviera <javiera.arc@hotmail.com>
2024-05-14 23:20:38 -0400

    troste

--- Tabla.py
+++ Tabla.py
@@ -1,5 +1,5 @@
 print ("Ingrese el numero el cual quiere saber la tabla hasta el 10")
 num = int (input())
-for i in range (11):
+for i in range (1,11):
     print (num, "x", i ,"=" , num*i)
 
---
+++ repaso/contaroalabras.py
@@ -0,0 +1,8 @@
+print  ("Escriber  una palabra")
+palabra =  input()
+contador = 0
+for i  in  range  (len(palabra)):
+    contador+=1
+print  (f"La  cantidad de letras  que  tiene  {palabra}    es: ",i+1)
+
+
