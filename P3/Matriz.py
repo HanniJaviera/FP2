7801976c97b3c7fe8b0901219e51cc2308cfa5d4
hannijaviera <javiera.arc@hotmail.com>
2024-05-23 20:18:01 -0400

    lista

---
+++ P3/Matriz.py
@@ -0,0 +1,12 @@
+matriz_sencilla = [
+[1,2,3],
+[4,5,6]
+]
+
+print (matriz_sencilla[0][1])
+
+for elemento in matriz_sencilla:
+    print (elemento)
+
+#Se puede tener una lista dentro de otra lista
+#   Como acceder a un elemento en especifico 
---
+++ P3/ejercicios/1.py
@@ -0,0 +1,12 @@
+#Escribe un programa que permta almacenar 3 nombres solicitados por pantalla en una lista, luego
+# el sistema deberá mostrar el nombre que tenga mayor cantidad de caracteres en un mensaje de salida por pantalla
+
+lista_nombres = []
+
+for nombres in range (3):
+  nombre = input (str("Escribe 3 nombres "))
+  lista_nombres.append (nombre);
+  print (lista_nombres)
+
+for lista_nombre in len(nombre):
+  print (nombre)
