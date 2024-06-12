8541c488614614f1179e4d99b2087b16a341c338
hannijaviera <javiera.arc@hotmail.com>
2024-06-11 20:21:33 -0400

    11-06

--- P3/ejercicios/ejerciciofun.py
+++ P3/ejercicios/ejerciciofun.py
@@ -1,4 +1,6 @@
 lista= [1,2,3,4,5,6,7,8,9,10]
+midiccionario ={'a': 1, 'b' : 2, 'c' : 3}
+
 
 def recorridolista():
     for elemento in (lista):
@@ -7,8 +9,14 @@ def recorridolista():
 recorridolista()
 
 
-def pareimpar(recorridolista):
-   for elemento in lista:
-        
+def buscarvalor (diccionario,key):
+   if key in diccionario:
+      return diccionario[key]
+   else:
+      return f"La clave '{key}' no existe en el diccionario."
 
-# %2==0 ES PAR 
+clavebuscada = str(input("Ingrese numero indice: "))
+
+resultado = buscarvalor (midiccionario, clavebuscada)
+print (resultado)
+      
